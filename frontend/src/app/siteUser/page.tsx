08db501d4740f42d0af05422cb20b9951bc43e37
easy0716 <dldmsgktjf0716@gmail.com>
2024-09-12 14:31:16 +0900

    Remove submodule and add frontend as regular directory

---
+++ frontend/src/app/siteUser/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function SiteUser() {
+    return <div>
+        <div className="bg-red-500"></div>
+        <div className="text-red-500 bg-gray-100 p-4 m-2">
+            Hello, Tailwind!
+        </div>
+    </div>
+}
